d4a840b7eed0374bc70907a97fdb9497d21a3105
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-03-05 15:46:01 +0300

    add plots output for minimisation function

--- AlgorithmScripts.py
+++ AlgorithmScripts.py
@@ -1,4 +1,7 @@
-# ДОБАВИТЬ ГРАФИК
+import numpy
+import matplotlib.pyplot as plot
+
+
 def dichotomy_find_min(interval: list, delta, epsilon, function) -> tuple:
     x_min: float
     iterations = 0
@@ -13,7 +16,7 @@ def dichotomy_find_min(interval: list, delta, epsilon, function) -> tuple:
     else:
         x_min = sum(interval) / 2
     convergence = 1 / (pow(2, iterations / 2))
-    return ("x_min = " + str(x_min), "interval = " + str(interval),
+    return ("x_min = " + str(x_min), "f(x_min) = " + str(function(x_min)), "interval = " + str(interval),
             "iterations = " + str(iterations), "convergence = " + str(convergence))
 
 
@@ -22,3 +25,7 @@ L = [-2, 8]
 eps = 0.2
 l = 0.5
 print(dichotomy_find_min(L, eps, l, f))
+
+x = numpy.arange(L[0]-1, L[1]+1, 0.01)
+plot.plot(x, f(x))
+plot.show()
